f4b3ab76b10d68416fdb6a1b4eac476311b88c22
Bùi Thế Vĩnh Nguyên <btvn256@gmail.com>
2024-05-20 14:22:23 +0700

    upload files

---
+++ DSA/CHELLO - Hello World.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long a,b,m,k;
+long long ngang, doc, tmp, tmp2;
+
+void solve()
+{
+   cout << "Hello PTIT.";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    int t = 1; 
+    // cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+}
