943e3c5e0b78b3cea0c7d18a9fbdb2de37649916
Bùi Thế Vĩnh Nguyên <btvn256@gmail.com>
2024-05-20 14:32:32 +0700

    upload files

---
+++ DSA/DSA09015 - KIỂM TRA CHU TRÌNH TRÊN ĐỒ THỊ CÓ HƯỚNG.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define FOR(i,a,b) for(int i = a; i < b; i++)
+#define FORD(i,a,b) for(int i = a; i > b; i--) 
+#define fi first
+#define se second
+#define pb push_back
+#define TEST cin >> t; while(t--)
+#define MOD 1000000007
+
+int n, m;
+vector<int> ke[1005];
+int color[1005];
+int check[1005];
+
+bool DFS(int u)
+{
+    color[u] = 1; //xam
+    for(auto i : ke[u])
+    {
+        if(color[i] == 0)
+        {
+            if(DFS(i)) return true;
+        }
+        else if(color[i] == 1) return true;
+    }
+    color[u] = 2; //den
+    return false;
+}
+
+void solve()
+{
+    FOR(i,0,1003) ke[i].clear();
+    memset(color,0,sizeof(color));
+    cin >> n >> m;
+    int x, y;
+    FOR(i,0,m) 
+    {
+        cin >> x >> y;
+        ke[x].pb(y);
+        // ke[y].pb(x);
+    }
+    FOR(i,1,n+1) 
+    {
+        if(color[i] == 0)
+        {
+            if(DFS(i))
+            {
+                cout << "YES\n";
+                return;
+            }
+        }
+    }
+    cout << "NO\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    TEST   
+    {
+        solve();
+    }
+}
